465906e2dad533f2e1ba91a0cdc5a869f3009f81
nguyen981 <nguyen29082004@gmail.com>
2024-05-29 14:17:10 +0700

    init projectt

---
+++ Controllers/BdnSongController.cs
@@ -0,0 +1,34 @@
+﻿using BdnLesson06.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BdnLesson06.Controllers
+{
+    public class BdnSongController : Controller
+    {
+        private static List<BdnSong> bdnSongs = new List<BdnSong>() 
+        {
+            new BdnSong{Id=2200123,BdnTitle="Bui Duc Nguyen", BdnAuthor="K22CNT3",BdnArtist="NTU", BdnYearRelease=2020 },
+            new BdnSong{Id=1,BdnTitle="Bui Duc Nguyen", BdnAuthor="K22CNT3",BdnArtist="NTU", BdnYearRelease=2020 }
+        };
+        // GET: BdnSong
+        public ActionResult BdnIndex()
+        {
+            return View(bdnSongs);
+        }
+        //Get : BdnCreate
+        /// <summary>
+        /// Form theem moi bai hat
+        /// Author: Bui Duc Nguyen
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult BdnCreate()
+        {
+            var bdnSong = new BdnSong();
+            return View();
+        }
+    }
+}
---
+++ Models/BdnSong.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BdnLesson06.Models
+{
+    public class BdnSong
+    {
+        [Key] 
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Bdn: Hay nhap tieu de")]
+        [DisplayName("Tieu de")]
+        public string BdnTitle { get; set; }
+        [Required(ErrorMessage = "Bdn: Hay nhap tac gia")]
+        [DisplayName("tac gia")]
+        public string BdnAuthor { get; set; }
+        [Required(ErrorMessage = "Bdn: Hay nhap nghe si")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Bdn: ten nghe si co toi theiu 2 ky tu, toi da 50 ki tu")]
+        [DisplayName("nghe si")]
+        public string BdnArtist { get; set; }
+
+        [Required(ErrorMessage = "Bdn: Hay nhap nam xuat ban")]
+        [RegularExpression(@"[0-9]{4}", ErrorMessage =" Nhap xuat ban phai co 2 ky tu so")]
+        [Range(1900,2024, ErrorMessage ="Bdn: nam xuat ban trong khoang 1900-2024")]
+        [DisplayName("Nam xuat ban")]
+        public int BdnYearRelease { get; set; }
+    }
+}
